17087e095eb688a189189b4cffa70a7cf09cc00b
ZmachinskiiD <98962267+ZmachinskiiD@users.noreply.github.com>
2024-02-12 15:51:11 +0300

    Продолжаю рефакторить

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'vendor_name/buildinglist',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'd68b72c60ad6ca64ccb0527412c0d88c0615942a',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'vendor_name/buildinglist' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'd68b72c60ad6ca64ccb0527412c0d88c0615942a',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
