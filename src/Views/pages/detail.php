9bb917e04d01ed27bfec914c5fd4ff1566a203c4
Zmachinskii Dmitrii <zmachinskiiof@gmail.com>
2024-02-17 22:12:35 +0200

    ПаЧиНиЛ БаГ

--- src/Views/pages/detail.php
+++ src/Views/pages/detail.php
@@ -5,18 +5,18 @@
  */
 ?>
 <div class="building_card">
-    <div class="card_header">
-        <div class="rus_name">
+    <div class="building_card__card_header">
+        <div class="D_rus_name">
             <?=$building->getRusTitle()?>
         </div>
-        <div class="deu_name">
+        <div class="D_deu_name">
             <?=$building->getDeuTitle()?>
         </div>
     </div>
-    <div class="building_info">
+    <div class="D_building_info">
         <?=$building->getDescription()?>
     </div>
-    <div class="building_photos">
+    <div class="D_building_photos">
         <?php foreach($buildingPhotos as $buildingPhoto):?>
             <img src="<?="./BuildingPhotos/".$buildingPhoto?> " height="400">
         <?php endforeach;?>
